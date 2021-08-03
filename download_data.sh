c3a10290cdfc7322d566348d3437408d7800f0f7
Edward Smith <ejsmith@fb.com>
2021-08-03 13:56:44 -0400

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -1,3 +1,8 @@
+#Copyright (c) Facebook, Inc. and its affiliates.
+#All rights reserved.
+#This source code is licensed under the license found in the
+#LICENSE file in the root directory of this source tree. 
+
 wget https://dl.fbaipublicfiles.com/ABC/data.tar.gz
 tar xf data.tar.gz -C data --strip-components=1
 rm -rf data.tar.gz
